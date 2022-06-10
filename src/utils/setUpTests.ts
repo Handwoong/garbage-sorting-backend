bcea055e16ed68cf86ef4508b473abaa44cf8527
Handwoong <handwoong@gmail.com>
2022-06-11 01:13:21 +0900

    [#19] test: CI 최종 테스트

--- src/utils/setUpTests.ts
+++ src/utils/setUpTests.ts
@@ -2,33 +2,19 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import server from "@src/server";
 import { MongoMemoryServer } from "mongodb-memory-server";
-jest.useRealTimers();
 
-const dbConnect = async () => {
+beforeAll(async () => {
     if (process.env.NODE_ENV === "test") {
         const mongoServer = await MongoMemoryServer.create();
         await mongoose.connect(mongoServer.getUri());
     }
-};
-
-const dbClear = async () => {
-    await mongoose.connection.db.dropDatabase();
-};
-
-const dbDisconnect = async () => {
-    await mongoose.disconnect();
-};
-
-beforeAll(async () => {
-    jest.setTimeout(30000);
-    await dbConnect();
 });
 
 beforeEach(async () => {
-    await dbClear();
+    await mongoose.connection.db.dropDatabase();
 });
 
 afterAll(async () => {
     server.close();
-    await dbDisconnect();
+    await mongoose.disconnect();
 });
